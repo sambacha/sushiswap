2c0b89c90f0cc91becca53ff64d34f294aaf31a6
Vasu Gupta <vasu99g@uw.edu>
2021-03-28 14:55:18 -0700

    override public -> public override

--- spec/harnesses/MasterChefV2SimplifiedHarness.sol
+++ spec/harnesses/MasterChefV2SimplifiedHarness.sol
@@ -15,12 +15,12 @@ contract MasterChefV2SimplifiedHarness is MasterChefV2Harness {
     ////////////////////////////////////////////////////////////////////////////
 
     mapping(uint256 => mapping(uint64 => mapping( uint256 => uint256))) symbolicSushiReward; // random number
-    function calculateSushiReward(uint256 blocks, uint64 poolAllocPoint) override public returns (uint256) {
+    function calculateSushiReward(uint256 blocks, uint64 poolAllocPoint) public override returns (uint256) {
         return symbolicSushiReward[blocks][poolAllocPoint][totalAllocPoint];
     }
 
     mapping(uint256 => mapping(uint256 => uint256)) symbolicSushiPerShare; // random number
-    function calculateSushiPerShare(uint256 sushiReward, uint256 lpSupply) override public returns (uint128) {
+    function calculateSushiPerShare(uint256 sushiReward, uint256 lpSupply) public override returns (uint128) {
         return (sushiReward.mul(ACC_SUSHI_PRECISION) / lpSupply).to128();
     }
 }
